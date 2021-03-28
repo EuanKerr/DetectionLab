920368f1be8f25b562b589eee73e216e5296192c
Euan Kerr <euan.kerr@pwc.com>
2021-03-28 15:17:34 +0100

    updates

--- ESXi/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {     
-  required_version = ">= 0.13"                                                                                                                                                                                                    
-  required_providers {                                                                                                                                                                                              
-    esxi = {                                                                                                                                                                                                        
-      source = "josenk/esxi"                                                                                                                                                                                        
-      version = "1.8.0"                                                                                                                                                                                             
-    }
-  }
-}
